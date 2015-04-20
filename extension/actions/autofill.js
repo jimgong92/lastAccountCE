3534b24c83b96fc367cce4af7370071f659783ce
Jimmy Gong <gong.jim@gmail.com>
2015-04-20 00:22:10 -0700

    Refactors autofill code to be more modular

--- extension/actions/autofill.js
+++ extension/actions/autofill.js
@@ -1,60 +1,64 @@
-var dummyData = {
+var userTemplate = {
+  "username": "jimgong92",
   "first": "Jimmy",
   "last": "Gong",
-  "username": "jimgong92",
+  "fullName": "Jimmy Gong",
   "email": "gong.jim@gmail.com"
-};
+}
 
-console.log('In autofill file');
-$(function() {
-  console.log('Running autofill');
-  var text = "hello jimmy";
-  //grab input tag
-  // var tag = document.getElementById("myInput")
-  // setTimeout(function(){tag.value = text}, 1000)
-  // var x = document.body.getElementByName("LABEL")
-  // console.log(x)
-  var labels = document.getElementsByTagName('LABEL');
-  var all = document.getElementsByTagName("*");
-  for(var i = 0; i < all.length; i++){
-    currentTag = all[i];
+$(function(){
+  var inputs = getCredentialInputs();
 
-    if(tagIsOfTypeInputAndTextOrEmail(currentTag)){
-      //1. check input tag name, id and placeholder for keywords
-      if(findTagNameKeyword(currentTag)){
-        var key = findTagNameKeyword(currentTag);
-        currentTag.value = dummyData[key]
-      }
-      //if above returns undefined run this
-      if(findTagIdKeyword(currentTag)){
-        var key = findTagIdKeyword(currentTag);
-        currentTag.value = dummyData[key]
-      }
+  inputs.each(function(tagId, tag){
+    if (isCredentialTag(tag, ['username', 'user-name'])){
+      tag.value = userTemplate['username'];
     }
-  }
+    else if (isCredentialTag(tag, 'first')){
+      tag.value = userTemplate['first'];
+    }
+    else if (isCredentialTag(tag, 'last')){
+      tag.value = userTemplate['last'];
+    }
+    else if (isCredentialTag(tag, 'name')){
+      tag.value = userTemplate['name'];
+    }
+    else if (isCredentialTag(tag, ['email', 'e-mail'])){
+      tag.value = userTemplate['email'];
+    }
+  });
 });
-
-function tagIsOfTypeInputAndTextOrEmail(tag){
-  return tag.tagName === "INPUT" && (tag.type === "text" || tag.type === 'email');
-}
-
-function findTagNameKeyword(tag){
-  var keyword = tag.name;
-  var element = findEquivalentKeyword(keyword);
-  return element;
-}
-
-function findTagIdKeyword(tag){
-  var keyword = tag.id;
-  var element = findEquivalentKeyword(keyword);
-  return element;
-}
-
-function findEquivalentKeyword(key){
-  var key = key.toLowerCase();
-  for(var element in dummyData){
-    if(key.indexOf(element) !== -1){
-      return element;
+/**
+ * @params: tag (DOM element), keys (Array[String])
+ * Returns boolean regarding whether tag contains the key type (e.g. username, password)
+ */
+function isCredentialTag(tag, keys){
+  if (!Array.isArray(keys)){
+    keys = [keys];
+  }
+  var placeholder = tag.placeholder.toLowerCase(),
+      name = tag.name.toLowerCase(),
+      id = tag.id.toLowerCase(),
+      className = $(tag).attr('class');
+  var attributes = [placeholder, name, id, className];
+  for (var i = 0; i < attributes.length; i++){
+    if (attributes[i]){
+      for (var j = 0; j < keys.length; j++){
+        if (attributes[i].indexOf(keys[j]) !== -1){
+          return true;
+        }
+      }
     }
   }
+  return false;
 }
+/**
+ * Returns all text input tags
+ */
+ function getCredentialInputs(){
+   return $('input').filter(function(tagId, tag){
+    if (tag.type === 'email'){
+      tag.value = userTemplate['email'];
+    }
+    return tag.type === 'text';
+  });
+ } 
