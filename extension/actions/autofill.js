31ddab154303bb9ce2e8fa5a146d9d51835a356a
Jimmy Gong <gong.jim@gmail.com>
2015-04-21 11:30:33 -0700

    Adds template to chrome storage

--- extension/actions/autofill.js
+++ extension/actions/autofill.js
@@ -1,30 +1,34 @@
-var userTemplate = {
-  "username": "jimgong92",
-  "first": "Jimmy",
-  "last": "Gong",
-  "fullName": "Jimmy Gong",
-  "email": "gong.jim@gmail.com"
-}
+// var dummyData = {
+//   "username": "jimgong92",
+//   "first": "Jimmy",
+//   "last": "Gong",
+//   "fullName": "Jimmy Gong",
+//   "email": "gong.jim@gmail.com",
+// }
+// chrome.storage.sync.set({'lastAccountTemplate': dummyData});
+
 
 $(function(){
-  var inputs = getCredentialInputs();
-
-  inputs.each(function(tagId, tag){
-    if (isCredentialTag(tag, ['username', 'user-name'])){
-      tag.value = userTemplate['username'];
-    }
-    else if (isCredentialTag(tag, 'first')){
-      tag.value = userTemplate['first'];
-    }
-    else if (isCredentialTag(tag, 'last')){
-      tag.value = userTemplate['last'];
-    }
-    else if (isCredentialTag(tag, 'name')){
-      tag.value = userTemplate['name'];
-    }
-    else if (isCredentialTag(tag, ['email', 'e-mail'])){
-      tag.value = userTemplate['email'];
-    }
+  chrome.storage.sync.get('lastAccountTemplate', function(item){
+    var template = item['lastAccountTemplate'];
+    var inputs = getCredentialInputs(template);
+    inputs.each(function(tagId, tag){
+      if (isCredentialTag(tag, ['username', 'user-name'])){
+        tag.value = template['username'] || '';
+      }
+      else if (isCredentialTag(tag, 'first')){
+        tag.value = template['first'] || '';
+      }
+      else if (isCredentialTag(tag, 'last')){
+        tag.value = template['last'] || '';
+      }
+      else if (isCredentialTag(tag, 'name')){
+        tag.value = template['name'] || '';
+      }
+      else if (isCredentialTag(tag, ['email', 'e-mail'])){
+        tag.value = template['email'] || '';
+      }
+    });
   });
 });
 /**
@@ -54,10 +58,10 @@ function isCredentialTag(tag, keys){
 /**
  * Returns all text input tags
  */
- function getCredentialInputs(){
+ function getCredentialInputs(template){
    return $('input').filter(function(tagId, tag){
     if (tag.type === 'email'){
-      tag.value = userTemplate['email'];
+      tag.value = template['email'];
     }
     return tag.type === 'text';
   });
